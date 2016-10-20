2992e7a14de3956f1e171df7d062633a51433066
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2016-10-20 08:32:09 +0200

    Add simple check to make sure the uploaded file is a image.

--- ftw/sliderblock/tests/test_uploadcapable.py
+++ ftw/sliderblock/tests/test_uploadcapable.py
@@ -3,6 +3,8 @@ from ftw.builder import Builder
 from ftw.builder import create
 from ftw.sliderblock.browser import uploadcapable
 from ftw.sliderblock.testing import FTW_SLIDERBLOCK_FUNCTIONAL_TESTING
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
 from unittest2 import TestCase
 from zope.component import queryAdapter
 
@@ -14,6 +16,8 @@ class TestUploadCapableAdapter(TestCase):
     def setUp(self):
         super(TestUploadCapableAdapter, self).setUp()
         self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager', 'Site Administrator'])
+
         self.page = create(Builder('sl content page').titled(u'A page'))
         self.sliderblock = create(Builder('sliderblock')
                                   .within(self.page))
@@ -24,15 +28,26 @@ class TestUploadCapableAdapter(TestCase):
         self.assertIsInstance(
             adapter, uploadcapable.SliderBlockQuickUploadCapableFileFactory)
 
-    def test_portal_type_is_alway_a_file(self):
-        upload = queryAdapter(self.filelistingblock, IQuickUploadFileFactory)
+    def test_portal_type_is_alway_a_slider_pane(self):
+        upload = queryAdapter(self.sliderblock, IQuickUploadFileFactory)
         upload('test.jpg',
                'File title',
                'File description',
                'image/jpeg',
                'DATA',
-               None)  # portal_type should be forced as 'File'.
-        contents = self.filelistingblock.objectValues()
+               None)   # portal_type should be forced as 'ftw.slider.Pane'.
+        contents = self.sliderblock.objectValues()
 
         self.assertEquals(1, len(contents), 'Expect exactly one item')
         self.assertEquals('ftw.slider.Pane', contents[0].portal_type)
+
+    def test_invalid_image_type(self):
+        upload = queryAdapter(self.sliderblock, IQuickUploadFileFactory)
+
+        with self.assertRaises(ValueError):
+            upload('test.pdf',
+                   'File title',
+                   'File description',
+                   'application/pdf',
+                   'DATA',
+                   None)
