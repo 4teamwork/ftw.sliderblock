6a4de22ba34213d9b5acad5db6594322ee10f534
Elio Schmutz <e.schmutz@4teamwork.ch>
2018-09-19 12:03:09 +0200

    Set required ftw.simplelayout version

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     zip_safe=False,
 
     install_requires=[
-        'ftw.simplelayout [contenttypes] >= 1.12.0',
+        'ftw.simplelayout [contenttypes] > 1.21.1',
         'ftw.slider >= 3.1.2',
         'ftw.upgrade',
         'setuptools',
