007e658f27770dcdd840f89fc64d6be7faecd72f
Jonas Baumann <jone@jone.ch>
2019-12-17 18:45:12 +0100

    Use ftw.slider >= 3.4.1
    We need https://github.com/4teamwork/ftw.slider/pull/94

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
 
     install_requires=[
         'ftw.simplelayout [contenttypes] >= 2.0.0',
-        'ftw.slider >= 3.1.5',
+        'ftw.slider >= 3.4.1',
         'ftw.upgrade',
         'setuptools',
         'plone.api',
